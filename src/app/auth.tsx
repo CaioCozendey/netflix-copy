761f51230786daefa25d90bba7e29d8c0220f7db
Caio Dias <caiocozendey@hotmail.com>
2023-07-13 21:28:29 -0300

    first commit

---
+++ src/app/auth.tsx
@@ -0,0 +1,7 @@
+const Auth = () =>{
+    return(
+        <div className="relative h-full w-full bg-[url('/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            
+        </div>
+    )
+}
